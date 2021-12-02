e777093c8c85939531ed4f5adb0c4133dd0f7551
Mercia Malan <mercia.malan@thameswater.co.uk>
2021-12-02 13:02:30 +0200

    Add unit tests for day 1

--- 2021/submarine.py
+++ 2021/submarine.py
@@ -50,12 +50,14 @@ def sonar_sweep_sliding_window(df):
 
   df[prev_depth] = df[depth].shift(1)
   df[prev_prev_depth] = df[prev_depth].shift(1)
-  df["depth_calculation"] = df[depth] + df[prev_depth] + df[prev_prev_depth]
+  df["depth_sum"] = df[depth] + df[prev_depth] + df[prev_prev_depth]
+  df["prev_depth_sum"] = df["depth_sum"].shift(1)
+  df["depth_calculation"] = df["depth_sum"] - df["prev_depth_sum"]
   
   return df
     
     
 def calculate_number_of_sea_floor_depth_increases(df):
-  diffs = sonar_sweep_basic(df)["depth_calculation"]
+  diffs = df["depth_calculation"]
       
   return diffs[diffs > 0].count()
--- 2021/tests/test.py
+++ 2021/tests/test.py
@@ -9,15 +9,18 @@ importlib.reload(common)
 class MainTests:
     def day1_part1(df):
         df_sweep = sonar_sweep_basic(df)
-        increases = calculate_number_of_sea_floor_depth_increases(df_sweep)
+        increases_basic = calculate_number_of_sea_floor_depth_increases(df_sweep)
+        expected_basic_increases = 1482
     
-        assert increases == 1482, f"Increases should be 1482, found {increases}" 
-    
+        assert increases_basic == expected_basic_increases, f"Basic Increases should be {expected_basic_increases}, found {increases_basic}" 
+
     def day1_part2(df):
         df_sw_sweep = sonar_sweep_sliding_window(df)
-        increases = calculate_number_of_sea_floor_depth_increases(df_sw_sweep)
+        increases_window = calculate_number_of_sea_floor_depth_increases(df_sw_sweep)
+        expected_window_increases = 1518
     
-        assert increases == 1518, f"Increases should be 1518, found {increases}" 
+        assert increases_window == expected_window_increases, f"Window Increases should be {expected_window_increases}, found {increases_window}" 
+    
     
     def day2_part1(instructions):
         submarine = Submarine(0,0,0)
@@ -51,14 +54,32 @@ class UnitTests:
         assert depth_calc == expected_depth_calc, f"Sum of depth differences expected to be {expected_depth_calc}, but found {depth_calc}"
         assert expected_df_shape_after_transform == sonar_sweep_shape, f"Expected df to be {expected_df_shape_after_transform} (rows, cols) but found {sonar_sweep_shape}" 
 
+    def test_sliding_window_sweep(df):
+        sonar_sweep = sonar_sweep_sliding_window(df)
+        sonar_sweep_shape = sonar_sweep.shape
+        depth_sum = sonar_sweep["depth_sum"].sum()
+        depth_calc = sonar_sweep["depth_calculation"].sum()
+
+        expected_depth_sum_sum = 5384 
+        expected_depth_calc_sum = 185 
+        expected_df_shape_after_transform = (10,6)
+
+        # print(sonar_sweep.columns)
+
+        assert depth_sum == expected_depth_sum_sum, f"Sum of depth sums expected to be {expected_depth_sum_sum}, but found {depth_sum}"
+        assert depth_calc == expected_depth_calc_sum, f"Sum of depth differences expected to be {expected_depth_calc_sum}, but found {depth_calc}"
+        assert expected_df_shape_after_transform == sonar_sweep_shape, f"Expected df to be {expected_df_shape_after_transform} (rows, cols) but found {sonar_sweep_shape}" 
+        
+
 
 # Run All
 def run_day1_tests():
     day1_input = get_df_from_file('input/day1.txt', None, ['depth'])
     day1_sample_input = get_df_from_file('input/day1_sample.txt', None, ['depth'])
     UnitTests.test_main_sweep(day1_sample_input)
-    #MainTests.day1_part1(day1_input)
-    #MainTests.day1_part2(day1_input)
+    UnitTests.test_sliding_window_sweep(day1_sample_input)
+    MainTests.day1_part1(day1_input)
+    MainTests.day1_part2(day1_input)
 
 def run_day2_tests():
     day2_input = read_file('input/Day2.txt')
