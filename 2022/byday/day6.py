26bc8b91cdcdb801c1b8dfb32cd5d4b79e81769f
Mercia Malan <mercia.malan@thameswater.co.uk>
2022-12-05 22:44:51 +0200

    prep day 6

---
+++ 2022/byday/day6.py
@@ -0,0 +1,15 @@
+def part1(input_list):
+
+
+
+
+
+    print(1)
+
+def part2(input_list):
+    print(2)
+
+sample = open('input/day6_sample.txt').readlines()
+full = open('input/day6.txt').readlines()
+part1(sample)
+# part2(sample)
