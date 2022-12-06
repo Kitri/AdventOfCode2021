3270a124e06e3557e7ece026968f43d67f75ccf2
Mercia Malan <mercia.malan@thameswater.co.uk>
2022-12-06 07:32:18 +0200

    day6 take 1

--- 2022/byday/day6.py
+++ 2022/byday/day6.py
@@ -1,15 +1,37 @@
-def part1(input_list):
+def find_marker(input_str,num_letters):
+    x = []
+    counter = 0
+    idx = 0
+    for c in input_str:
+        idx += 1
+        if c not in x:
+            counter += 1
+            x.append(c)
+        else:
+            counter = 0
+            c_index = x.index(c)
+            
+            x = x[c_index+1:]
+            x.append(c)
 
+        if len(x) == num_letters:
+            return idx
 
+sample = [x.strip() for x in open('input/day6_sample.txt').readlines()]
+full = [x.strip() for x in open('input/day6.txt').readlines()]
 
+assert find_marker(sample[0],4) == 7
+assert find_marker(sample[1],4) == 5
+assert find_marker(sample[2],4) == 6
+assert find_marker(sample[3],4) == 10
+assert find_marker(sample[4],4) == 11
 
+print('Part1 ', find_marker(full[0],4))
 
-    print(1)
+assert find_marker(sample[0],14) == 19
+assert find_marker(sample[1],14) == 23
+assert find_marker(sample[2],14) == 23
+assert find_marker(sample[3],14) == 29
+assert find_marker(sample[4],14) == 26
 
-def part2(input_list):
-    print(2)
-
-sample = open('input/day6_sample.txt').readlines()
-full = open('input/day6.txt').readlines()
-part1(sample)
-# part2(sample)
+print('Part1 ', find_marker(full[0],14))
